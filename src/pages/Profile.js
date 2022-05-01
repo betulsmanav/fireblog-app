a56e89cc0f7275c0514d28e835c819003efa056d
betul <betulsmanav@gmail.com>
2022-05-01 11:06:35 +0300

    profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import { AuthContext } from "../contexts/AuthContext";
+import "./Profile.css";
+import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
+import { useContext } from "react";
 
 const Profile = () => {
-  return (
-    <div>
-      profile
-    </div>
-  )
-}
+  const { currentUser } = useContext(AuthContext)
 
-export default Profile
+  return (
+    <div className="profilContainer">
+      <div>
+        <AccountCircleRoundedIcon className="profileIcon" />
+        <p>User Name : {currentUser?.displayName}</p>
+        <p>User Email: {currentUser?.email}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
